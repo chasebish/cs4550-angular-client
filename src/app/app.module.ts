3a789ffed413a442d9f0185634f5050caf8b5da3
chasebish <bishop.c@husky.neu.edu>
2018-08-07 13:46:10 -0400

    heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { WhiteboardComponent } from './whiteboard/whiteboard.component';
-import CourseServiceClient from './services/course.service.client';
+import { CourseServiceClient } from './services/course.service.client';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
-import UserServiceClient from './services/user.service.client';
+import { UserServiceClient } from './services/user.service.client';
 
 @NgModule({
   declarations: [
