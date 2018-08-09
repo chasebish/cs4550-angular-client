3b21580b28af3f5880612177c43746edf0473f9e
chasebish <bishop.c@husky.neu.edu>
2018-08-09 15:04:27 -0400

    homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseServiceClient } from '../services/course.service.client';
+import { UserServiceClient } from '../services/user.service.client';
+import { EnrollServiceClient } from '../services/enroll.service.client';
 
 @Component({
     selector: 'app-home',
@@ -9,12 +11,30 @@ import { CourseServiceClient } from '../services/course.service.client';
 export class HomeComponent implements OnInit {
 
     courses: any = [];
+    user: any = {};
+    userSections: any = [];
 
-    constructor(private courseService: CourseServiceClient) { }
+    constructor(private courseService: CourseServiceClient, private userService: UserServiceClient,
+        private enrollService: EnrollServiceClient) { }
 
     ngOnInit() {
         this.courseService.findAllCourses()
             .then(courses => this.courses = courses);
+        this.userService.currentUser()
+            .then(user => {
+                this.user = user;
+                this.enrollService.studentSections(this.user._id)
+                    .then(enrollments => this.enrollmentsToSections(enrollments));
+            }, () => { return; });
+    }
+
+    enrollmentsToSections = enrollments => {
+        this.userSections = enrollments.map(enrollment => {
+            const section = enrollment.sectionId;
+            section.enrollmentId = enrollment._id;
+            return section;
+        });
+        console.log(this.userSections);
     }
 
 }
