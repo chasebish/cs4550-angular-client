c2a1717c529e74cf8682fb63e15f07df4b027039
chasebish <bishop.c@husky.neu.edu>
2018-08-09 21:23:17 -0400

    styling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '../../../node_modules/@angular/router';
+import { UserServiceClient } from '../services/user.service.client';
 
 @Component({
     selector: 'app-navbar',
@@ -9,18 +10,30 @@ import { Router } from '../../../node_modules/@angular/router';
 export class NavbarComponent implements OnInit {
 
     navbar = false;
+    user: any = {};
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private userService: UserServiceClient) { }
 
     ngOnInit() {
+        this.getUser();
+    }
+
+    getUser = () => {
+        this.userService.currentUser()
+            .then(user => this.user = user, () => this.user = this.user);
     }
 
     toggleNavbar = () => {
         this.navbar = !this.navbar;
     }
 
+    profile = () => this.router.navigate(['profile']);
     register = () => this.router.navigate(['register']);
     login = () => this.router.navigate(['login']);
-    logout = () => this.router.navigate(['login']);
+    logout = () => this.userService.logout()
+        .then(() => {
+            this.router.navigate(['login']);
+            this.getUser();
+        })
 
 }
