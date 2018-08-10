057ff0d13c74e1090add1b6de19b3eeeef2fb6a2
chasebish <bishop.c@husky.neu.edu>
2018-08-09 22:09:31 -0400

    compressed course-viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,9 @@ import { SectionServiceClient } from './services/section.service.client';
 import { HomeComponent } from './home/home.component';
 import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { CourseSectionsComponent } from './course-sections/course-sections.component';
-import { CourseModulesComponent } from './course-modules/course-modules.component';
 import { ModuleServiceClient } from './services/module.service.client';
-import { ModuleLessonsComponent } from './module-lessons/module-lessons.component';
-import { LessonTopicsComponent } from './lesson-topics/lesson-topics.component';
 import { LessonServiceClient } from './services/lesson.service.client';
 import { TopicServiceClient } from './services/topic.service.client';
-import { TopicWidgetsComponent } from './topic-widgets/topic-widgets.component';
 import { WidgetServiceClient } from './services/widget.service.client';
 import { AdminComponent } from './admin/admin.component';
 import { EnrollServiceClient } from './services/enroll.service.client';
@@ -38,10 +34,6 @@ import { NavbarComponent } from './navbar/navbar.component';
         HomeComponent,
         CourseViewerComponent,
         CourseSectionsComponent,
-        CourseModulesComponent,
-        ModuleLessonsComponent,
-        LessonTopicsComponent,
-        TopicWidgetsComponent,
         AdminComponent,
         NavbarComponent
     ],
--- src/app/course-viewer/course-viewer.component.ts
+++ src/app/course-viewer/course-viewer.component.ts
@@ -4,6 +4,7 @@ import { LessonServiceClient } from '../services/lesson.service.client';
 import { TopicServiceClient } from '../services/topic.service.client';
 import { WidgetServiceClient } from '../services/widget.service.client';
 import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { CourseServiceClient } from '../services/course.service.client';
 
 @Component({
     selector: 'app-course-viewer',
@@ -16,14 +17,11 @@ export class CourseViewerComponent implements OnInit {
     lessons: any = [];
     topics: any = [];
     widgets: any = [];
+
     courseId: String;
     moduleId: String;
     lessonId: String;
     topicId: String;
-
-    selectedModule: string;
-    selectedLesson: string;
-    selectedTopic: string;
 
     constructor(private moduleService: ModuleServiceClient, private lessonService: LessonServiceClient,
         private topicService: TopicServiceClient, private widgetService: WidgetServiceClient, private route: ActivatedRoute) { }
@@ -36,6 +34,36 @@ export class CourseViewerComponent implements OnInit {
             this.moduleService.findModulesForCourse(this.courseId)
                 .then(modules => this.modules = modules, () => { return; });
         }
+    }
+
+    selectModule = selectedModule => {
+        this.moduleId = selectedModule.id;
+        this.lessonService.findLessonsForModule(this.courseId, this.moduleId)
+            .then(lessons => {
+                this.lessons = lessons;
+                this.lessonId = undefined;
+                this.topicId = undefined;
+                this.topics = [];
+                this.widgets = [];
+            }, () => { return; });
+    }
+
+    selectLesson = selectedLesson => {
+        this.lessonId = selectedLesson.id;
+        this.topicService.findTopicsForLesson(this.courseId, this.moduleId, this.lessonId)
+            .then(topics => {
+                this.topics = topics;
+                this.topicId = undefined;
+                this.widgets = [];
+            }, () => { return; });
+    }
+
+    selectTopic = selectedTopic => {
+        this.topicId = selectedTopic.id;
+        this.widgetService.findWidgetsForTopic(this.courseId, this.moduleId, this.lessonId, this.topicId)
+            .then(widgets => {
+                this.widgets = widgets.sort((a, b) => a.widgetOrder - b.widgetOrder);
+            }, () => { return; });
     }
 
 }
